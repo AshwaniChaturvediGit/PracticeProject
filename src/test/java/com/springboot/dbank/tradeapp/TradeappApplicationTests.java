ff09db03fc468795563229be2f801b19cc2a10dd
Ashwani <ashwani_mhr4@yahoo.com>
2021-04-21 15:04:06 +0530

    Test Cases update

--- src/test/java/com/springboot/dbank/tradeapp/TradeappApplicationTests.java
+++ src/test/java/com/springboot/dbank/tradeapp/TradeappApplicationTests.java
@@ -1,125 +1,16 @@
 package com.springboot.dbank.tradeapp;
 
-import java.time.LocalDate;
-import java.util.List;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import com.springboot.dbank.tradeapp.exception.InvalidTradeStoreException;
-import com.springboot.dbank.tradeapp.model.Trade;
-import com.springboot.dbank.tradeapp.rest.TradeController;
-
-@SpringBootTest
+@SpringBootTest()
+@RunWith(SpringRunner.class)
 class TradeappApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-	@Autowired
-	private TradeController tradeController;
-
-	@Test
-	void testTradeStore_successful() {
-		ResponseEntity responseEntity = tradeController.tradeValidateStore(createMockTrade("T1",1,LocalDate.now()));
-		Assertions.assertEquals(ResponseEntity.status(HttpStatus.OK).build(),responseEntity);
-		List<Trade> tradeList =tradeController.findAllTrades();
-		Assertions.assertEquals(1, tradeList.size());
-		Assertions.assertEquals("T1",tradeList.get(0).getTradeId());
-	}
-
-	@Test
-	void testTradeStoreWhenMaturityDatePast() {
-		try {
-			LocalDate localDate = getLocalDate(2015, 05, 21);
-			ResponseEntity responseEntity = tradeController.tradeValidateStore(createMockTrade("T2", 1, localDate));
-		}catch (InvalidTradeStoreException ie) {
-			Assertions.assertEquals("Invalid Trade: T2  Trade Id is not found", ie.getMessage());
-		}
-	}
-
-	@Test
-	void testTradeStoreWhenOldVersion() {
-		// step-1 create trade
-		ResponseEntity responseEntity = tradeController.tradeValidateStore(createMockTrade("T1",2,LocalDate.now()));
-		Assertions.assertEquals(ResponseEntity.status(HttpStatus.OK).build(),responseEntity);
-		List<Trade> tradeList =tradeController.findAllTrades();
-		Assertions.assertEquals(1, tradeList.size());
-		Assertions.assertEquals("T1",tradeList.get(0).getTradeId());
-		Assertions.assertEquals(2,tradeList.get(0).getVersion());
-		Assertions.assertEquals("T1B1",tradeList.get(0).getBookId());
-
-		//step-2 create trade with old version
-		try {
-			ResponseEntity responseEntity1 = tradeController.tradeValidateStore(createMockTrade("T1", 1, LocalDate.now()));
-
-
-		}catch (InvalidTradeStoreException e){
-			System.out.println(e.getId());
-			System.out.println(e.getMessage());
-		}
-		List<Trade> tradeList1 =tradeController.findAllTrades();
-		Assertions.assertEquals(1, tradeList1.size());
-		Assertions.assertEquals("T1",tradeList1.get(0).getTradeId());
-		Assertions.assertEquals(2,tradeList1.get(0).getVersion());
-		Assertions.assertEquals("T1B1",tradeList.get(0).getBookId());
-	}
-
-	@Test
-	void testTradeStoreWhenSameVersionTrade(){
-		ResponseEntity responseEntity = tradeController.tradeValidateStore(createMockTrade("T1",2,LocalDate.now()));
-		Assertions.assertEquals(ResponseEntity.status(HttpStatus.OK).build(),responseEntity);
-		List<Trade> tradeList =tradeController.findAllTrades();
-		Assertions.assertEquals(1, tradeList.size());
-		Assertions.assertEquals("T1",tradeList.get(0).getTradeId());
-		Assertions.assertEquals(2,tradeList.get(0).getVersion());
-		Assertions.assertEquals("T1B1",tradeList.get(0).getBookId());
-
-		//step-2 create trade with same version
-		Trade trade2 = createMockTrade("T1",2,LocalDate.now());
-		trade2.setBookId("T1B1V2");
-		ResponseEntity responseEntity2 = tradeController.tradeValidateStore(trade2);
-		Assertions.assertEquals(ResponseEntity.status(HttpStatus.OK).build(),responseEntity2);
-		List<Trade> tradeList2 =tradeController.findAllTrades();
-		Assertions.assertEquals(1, tradeList2.size());
-		Assertions.assertEquals("T1",tradeList2.get(0).getTradeId());
-		Assertions.assertEquals(2,tradeList2.get(0).getVersion());
-		Assertions.assertEquals("T1B1V2",tradeList2.get(0).getBookId());
-
-		//step-2 create trade with new version
-		Trade trade3 = createMockTrade("T1",2,LocalDate.now());
-		trade3.setBookId("T1B1V3");
-		ResponseEntity responseEntity3 = tradeController.tradeValidateStore(trade3);
-		Assertions.assertEquals(ResponseEntity.status(HttpStatus.OK).build(),responseEntity3);
-		List<Trade> tradeList3 =tradeController.findAllTrades();
-		Assertions.assertEquals(1, tradeList3.size());
-		Assertions.assertEquals("T1",tradeList3.get(0).getTradeId());
-		Assertions.assertEquals(2,tradeList3.get(0).getVersion());
-		Assertions.assertEquals("T1B1V3",tradeList3.get(0).getBookId());
-
-	}
-	private Trade createMockTrade(String tradeId,int version,LocalDate  maturityDate){
-		Trade trade = new Trade();
-		trade.setTradeId(tradeId);
-		trade.setBookId(tradeId+"B1");
-		trade.setVersion(version);
-		trade.setCounterParty(tradeId+"Cpty");
-		trade.setMaturityDate(maturityDate);
-		trade.setExpiredFlag("Y");
-		return trade;
-	}
-
-	public static LocalDate getLocalDate(int year,int month, int day){
-		LocalDate localDate = LocalDate.of(year,month,day);
-		return localDate;
-	}
-
-
-	
-	
 }
